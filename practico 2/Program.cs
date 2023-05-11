e6c1eff50d71eeef8f828889956965fd274bfcb5
terco89 <valeraquiroz.gb@gmail.com>
2023-05-11 17:44:41 -0300

    practico 2 casi

---
+++ practico 2/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practico_2
+{
+    /*2) Haz una clase llamada Persona que siga las siguientes condiciones:
+Sus atributos son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura. No queremos que se accedan directamente a ellos. Piensa que modificador de acceso es el más adecuado, también su tipo. Si quieres añadir algún atributo puedes hacerlo.
+Por defecto, todos los atributos menos el DNI serán valores por defecto según su tipo (0 números, cadena vacía para String, etc.). Sexo sera hombre por defecto, usa una constante para ello.
+Se implantaran varios constructores:
+Un constructor por defecto.
+Un constructor con el nombre, edad y sexo, el resto por defecto.
+Un constructor con todos los atributos como parámetro.
+Los métodos que se implementarán son:
+calcularIMC(): calculará si la persona está en su peso ideal (peso en kg/(altura^2  en m)), si esta fórmula devuelve un valor menor que 20, la función devuelve un -1, si devuelve un número entre 20 y 25 (incluidos), significa que está por debajo de su peso ideal la función devuelve un 0  y si devuelve un valor mayor que 25 significa que tiene sobrepeso, la función devuelve un 1. Te recomiendo que uses constantes para devolver estos valores.
+esMayorDeEdad(): indica si es mayor de edad, devuelve un booleano.
+comprobarSexo(char sexo): comprueba que el sexo introducido es correcto. Si no es correcto, será H. No será visible al exterior.
+generaDNI(): genera un número aleatorio de 8 cifras, genera a partir de este su número su letra correspondiente. Este método será invocado cuando se construya el objeto. Puedes dividir el método para que te sea más fácil. No será visible al exterior.
+Métodos set de cada parámetro, excepto de DNI.
+Ahora, crea una clase ejecutable que haga lo siguiente:
+Pide por teclado el nombre, la edad, sexo, peso y altura.
+Crea 3 objetos de la clase anterior, el primer objeto obtendrá las anteriores variables pedidas por teclado, el segundo objeto obtendrá todos los anteriores menos el peso y la altura y el último por defecto, para este último utiliza los métodos set para darle a los atributos un valor.
+Para cada objeto, deberá comprobar si está en su peso ideal, tiene sobrepeso o por debajo de su peso ideal con un mensaje.
+Indicar para cada objeto si es mayor de edad.
+Por último, mostrar la información de cada objeto.
+Puedes usar métodos en la clase ejecutable, para que os sea mas fácil.
+*/
+    class Person
+    {
+        private string nombre = "Desconocido";
+        private int edad = 0, DNI,peso = 0,altura = 0;
+        private char sexo;
+
+        private const char dSexo = 'H';
+
+        public Person()
+        {
+            sexo = dSexo;
+            DNI = generarDNI();
+        }
+
+        public Person(string nombre, int edad, char sexo)
+        {
+            DNI = generarDNI();
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = comprobarSexo(sexo);
+        }
+
+        public Person(string nombre, int edad, char sexo, int peso, int altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = comprobarSexo(sexo);
+            this.altura = altura;
+            DNI = generarDNI();
+            this.peso = peso;
+        }
+
+        public int calcularIMC()
+        {
+            if (peso == 0 || altura == 0) {
+                return -1;
+            }
+            double IMC = (peso/1000) / ((altura/100) * (altura/100));
+
+            if (IMC < 20)
+            {
+                return -1;
+            }
+            else if (IMC >= 20 && IMC <= 25)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool esMayorDeEdad()
+        {
+            if (edad >= 18)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private char comprobarSexo(char sexo)
+        {
+            if (sexo == 'H' || sexo == 'M' || sexo == 'h' || sexo == 'm')
+            {
+                return sexo;
+            }
+            return dSexo;
+        }
+
+        private int generarDNI()
+        {
+            Random rd = new Random();
+            int dni = rd.Next(10000000, 99999999);
+            return dni;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set 
+            { 
+                edad = value;
+            }
+        }
+        public char Sexo
+        {
+            get
+            {
+                return sexo;
+            }
+            set
+            {
+                sexo = value;
+            }
+        }
+        public int Peso
+        {
+            get
+            {
+                return peso;
+            }
+            set
+            {
+                peso = value;
+            }
+        }
+        public int Altura
+        {
+            get
+            {
+                return altura;
+            }
+            set
+            {
+                altura = value;
+            }
+        }
+        public int dni
+        {
+            get
+            {
+                return DNI;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string nombre = "";
+            int edad = 0, DNI = 0,altura = 0,peso=0;
+            char sexo;
+
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad:");
+            edad = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su sexo: (M si es mujer y H si es hombre)");
+            sexo = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su altura en centimetros:");
+            altura = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su peso en gramos:");
+            peso = Convert.ToInt32(Console.ReadLine());
+
+            people.AddRange(new List<Person>(){new Person(nombre, edad, sexo, peso, altura), new Person(nombre,edad,sexo), new Person()});
+            Console.Clear();
+            Console.WriteLine("Se crearon tres personas\n");
+
+            int imc = 0;
+            foreach (Person person in people)
+            {
+                imc = person.calcularIMC();
+                if(imc == -1)
+                {
+                   Console.WriteLine("{0} esta por debajo de su peso ideal",person.Nombre);
+                }
+                else if (imc == 0)
+                {
+                    Console.WriteLine("{0} esta en su peso ideal",person.Nombre);
+                }
+                else
+                {
+                    Console.WriteLine("{0} tiene sobrepeso", person.Nombre);
+                }
+            }
+            Console.WriteLine("\n");
+            foreach (Person person in people)
+            {
+                if (person.esMayorDeEdad())
+                {
+                    Console.WriteLine("{0} es mayor de edad", person.Nombre);
+                }
+                else{
+                    Console.WriteLine("{0} es menor de edad", person.Nombre);
+                }
+            }
+            Console.WriteLine("\nInformación de las personas:\n");
+            foreach(Person person in people)
+            {
+                Console.WriteLine("Nombre: {0}\nEdad: {1}\nSexo: {2}\nAltura: {3}cm\nPeso: {4}kg\nDNI: {5}\n\n",person.Nombre,person.Edad,person.Sexo,person.Altura,person.Peso,person.dni);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ practico 4/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practico_4
+{
+    class Electrodomestico
+    {
+        private float precio_base = 100;
+        private string color = "blanco";
+        private char consumo_energetico = 'F';
+        private float peso = 5;
+
+        public Electrodomestico() { }
+
+        public Electrodomestico(float peso, float precio)
+        {
+            this.precio_base = precio;
+            this.peso = peso;
+        }
+        public Electrodomestico(float precio, string color, float peso, char consumo_energetico)
+        {
+            this.precio_base = precio;
+            this.peso = peso;
+            comprobarConsumoEnergetico(consumo_energetico);
+            comprobarColor(color);
+        }
+        public float Precio_base
+        {
+            get
+            {
+                return this.precio_base;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return this.color;
+            }
+        }
+        public char Consumo_energetico
+        {
+            get
+            {
+                return this.consumo_energetico;
+            }
+        }
+        public float Peso
+        {
+            get
+            {
+                return this.peso;
+            }
+        }
+        private void comprobarConsumoEnergetico(char c)
+        {
+            const string letras_posbles = "ABCDEF";
+            if (letras_posbles.Contains(c))
+            {
+                this.consumo_energetico = c;
+            }
+            else
+            {
+                this.consumo_energetico = 'F';
+            }
+        }
+        private void comprobarColor(string s)
+        {
+            const string colores_posibles = "BLANCONEGROROJOAZULGRIS";
+            if (colores_posibles.Contains(s.ToUpper()))
+            {
+                this.color = s.ToUpper();
+            }
+            else
+            {
+                this.color = "BLANCO";
+            }
+        }
+        public float precioFinal()
+        {
+            float precio = precio_base;
+            switch (this.consumo_energetico)
+            {
+                case 'A':
+                    precio_base += 100;
+                    break;
+                case 'B':
+                    precio_base += 80;
+                    break;
+                case 'C':
+                    precio_base += 60;
+                    break;
+                case 'D':
+                    precio_base += 50;
+                    break;
+                case 'E':
+                    precio_base += 30;
+                    break;
+                case 'F':
+                    precio_base += 10;
+                    break;
+            }
+            if (this.peso > 0 && this.peso < 19)
+            {
+                this.peso += 10;
+            }
+            else if (this.peso > 20 && this.peso < 49)
+            {
+                this.peso += 50;
+            }
+            else if (this.peso > 50 && this.peso < 79)
+            {
+                this.peso += 80;
+            }
+            else if (this.peso > 80)
+            {
+                this.peso += 100;
+            }
+
+            return precio;
+        }
+
+    }
+
+    class Lavadora : Electrodomestico
+    {
+        private int carga = 5;
+        public Lavadora() { }
+
+        public Lavadora(float peso, float precio) : base(peso, precio) { }
+        public Lavadora(float precio, string color, float peso, char consumo_energetico, int carga) : base(peso, color, peso, consumo_energetico)
+        {
+            this.carga = carga;
+        }
+        public int Carga
+        {
+            get
+            {
+                return this.carga;
+            }
+        }
+
+        public float precioFinal()
+        {
+            float precio = base.precioFinal();
+            if (this.carga > 30)
+            {
+                precio += 50;
+            }
+            return precio;
+        }
+    }
+    class Television : Electrodomestico
+    {
+        private float resolucion = 20;
+        private bool sintonizador = false;
+
+        public Television() : base() { }
+        public Television(float peso, float precio) : base(peso, precio) { }
+
+        public Television(float precio, string color, float peso, char consumo_energetico, float resolucion, bool sintonizador) : base(precio, color, peso, consumo_energetico)
+        {
+            this.sintonizador = sintonizador;
+            this.resolucion = resolucion;
+        }
+        public float Resolucion
+        {
+            get
+            {
+                return this.resolucion;
+            }
+        }
+        public bool Sintonizador
+        {
+            get
+            {
+                return this.sintonizador;
+            }
+        }
+
+        public float precioFinal()
+        {
+            float precio = base.precioFinal();
+            if (this.sintonizador)
+            {
+                precio += 50;
+            }
+            if (this.resolucion > 40)
+            {
+                precio *= 1.30F;
+            }
+            return precio;
+        }
+    }
+    class Ejecutable
+    {
+        static void Main(string[] args)
+        {
+            Electrodomestico[] electrodomesticos = new Electrodomestico[10];
+            electrodomesticos[0] = new Television(10, 15);
+            electrodomesticos[1] = new Lavadora(15, 58);
+            electrodomesticos[2] = new Television(10, "blanco", 45, 'D', 20, true);
+            electrodomesticos[3] = new Lavadora(85, "gris", 32, 'A', 29);
+            electrodomesticos[4] = new Lavadora();
+            electrodomesticos[5] = new Television();
+            electrodomesticos[6] = new Television(15, 65);
+            electrodomesticos[7] = new Lavadora(34, 77);
+            electrodomesticos[8] = new Lavadora(23, 32);
+            electrodomesticos[9] = new Television(98, "rojo", 65, 'B', 47, false);
+            List<Type> tipos = new List<Type>();
+            foreach (Electrodomestico e in electrodomesticos)
+            {
+                if (!tipos.Contains(e.GetType()))
+                {
+                    tipos.Add(e.GetType());
+                }
+            }
+            Console.WriteLine("La cantidad de tipos es: {0}", tipos.Count());
+            Console.ReadKey();
+
+            //Console.WriteLine("Televisores: {0} \n Lavarropas: {1} \n Electrodomesticos: {2}", televisores, lavarropas, televisores + lavarropas);
+
+        }
+    }
+}
