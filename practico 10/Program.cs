2dcece8632458fe9d936bae28f6824d7f2228730
terco89 <valeraquiroz.gb@gmail.com>
2023-06-02 11:56:03 -0300

    ya casi 10 lo juro

--- practico 10/Program.cs
+++ practico 10/Program.cs
@@ -21,24 +21,62 @@ namespace practico_10
                 "            Mostrar baraja     "
             };
             Menu menup = new Menu(opciones);
-            menup.pintar();
             while (true)
             {
-                ConsoleKeyInfo opcion = Console.ReadKey();
-                if (opcion.Key == ConsoleKey.DownArrow)
-                    menup.bajar();
-                else if (opcion.Key == ConsoleKey.UpArrow)
-                    menup.subir();
-                else if(opcion.Key == ConsoleKey.Enter)
+                menup.pintar();
+                while (true)
                 {
-                    if(menup.PosMenu == 0)
+                    ConsoleKeyInfo opcion = Console.ReadKey();
+                    if (opcion.Key == ConsoleKey.DownArrow)
+                        menup.bajar();
+                    else if (opcion.Key == ConsoleKey.UpArrow)
+                        menup.subir();
+                    else if (opcion.Key == ConsoleKey.Enter)
                     {
-                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        bool bandera = false;
+                        if (menup.PosMenu == 0)
+                        {
+                            baraja.barajar();
+                            Console.SetCursorPosition(30, 0);
+                            Console.Write("Se barajo las cartas");
+                            bandera = true;
+                        }
+                        else if (menup.PosMenu == 5)
+                        {
+                            Console.Clear();
+                            List<Carta> cartas = baraja.mostrarBaraja();
+                            Console.Write("Baraja actual");
+                            int x = 0, y = 0;
+                            for (int i = 0; i < cartas.Count; i++)
+                            {
+                                y++;
+                                if (i % 25 == 0)
+                                {
+                                    x++;
+                                    y = 0;
+                                }
+                                Console.SetCursorPosition(x*20, y + 2);
+                                Console.WriteLine(cartas[i].Numero + " de " + cartas[i].Palo);
+                            }
+                        }
+                        if (!bandera)
+                        {
+                            Console.SetCursorPosition(0,27);
+                            Console.Write("\nPresione esc para volver");
+                            ConsoleKeyInfo opcionA;
+                            do
+                            {
+                                opcionA = Console.ReadKey();
+                            } while (opcionA.Key != ConsoleKey.Escape);
+                            Console.Clear();
+                            break;
+                        }
                     }
                 }
             }
             
-            Console.ReadKey();
         }
     }
 }
